d22ab748f08a2ca44e83a02c72af553c9b3edf5a
Mvgikk <bartosikt11@gmail.com>
2024-10-31 17:40:19 +0100

    configure prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginTs from "@typescript-eslint/eslint-plugin";
+import parserTs from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["dist/**"],
+  },
+  
+  {files: ["**/*.js"], 
+    languageOptions: {sourceType: "commonjs"},    
+    rules: 
+    {
+      indent: ["error", 4],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "linebreak-style": ["error", "unix"],
+    }},
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      indent: ["error", 4],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "linebreak-style": ["error", "unix"],
+      "@typescript-eslint/no-unused-vars": "warn",
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+    plugins: {
+      "@typescript-eslint": pluginTs,
+    },
+  },
+  pluginJs.configs.recommended,
+
+];
